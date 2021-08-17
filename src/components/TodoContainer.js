d3d28bdc7cac2544d037bdf8f80088c3bd70fae7
carloso0114 <carlossospinag@gmail.com>
2021-08-16 20:47:47 -0500

    update states of checkboxes

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,5 +1,6 @@
 import React from "react"
 import TodosList from "./TodosList";
+import Header from "./Header"
 
 class TodoContainer extends React.Component {
   state = {
@@ -21,10 +22,26 @@ class TodoContainer extends React.Component {
       }
     ]
   }
+  handleChange = id => {
+    this.setState(prevState => {
+      return {
+        todos: prevState.todos.map(todo => {
+          if (todo.id === id) {
+            return {
+              ...todo,
+              completed: !todo.completed,
+            }
+          }
+          return todo
+        }),
+      }
+    })
+  };
   render() {
     return (
       <div>
-        <TodosList todos={this.state.todos} />
+        <Header />
+        <TodosList todos={this.state.todos} handleChangeProps={this.handleChange} />
       </div>
     );
   }
