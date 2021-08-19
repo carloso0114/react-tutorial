1b1e44b930aa73f2934180c8d62f9b5588d3f83e
carloso0114 <carlossospinag@gmail.com>
2021-08-19 17:10:19 -0500

    add nav links and nested links

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom"
 import TodoContainer from "./functionBased/components/TodoContainer"
 import "./functionBased/App.css"
+import { BrowserRouter as Router } from "react-router-dom"
 
-ReactDOM.render(<TodoContainer />, document.getElementById("root"))
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <TodoContainer />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+)
